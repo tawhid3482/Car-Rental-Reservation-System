d5f5ed784f20a7483e44713b0c6fec6817661805
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-12 21:53:39 +0600

    Your commit message

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,15 @@ import app from "./app";
 let server: Server;
 
 async function main() {
-  try {
-    await mongoose.connect(config.database_url as string);
+  // try {
+  //   await mongoose.connect(config.database_url as string);
 
-    server = app.listen(config.port, () => {
-      console.log(`app is listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  //   server = app.listen(config.port, () => {
+  //     console.log(`app is listening on port ${config.port}`);
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 }
 
 main();
