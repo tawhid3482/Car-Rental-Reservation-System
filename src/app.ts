6b386faf64d9e9569f7fd3f7a8c19c95d83c34e1
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-13 14:54:01 +0600

    by

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,8 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+// app.use(cookieParser());
+app.use(cors());
 
 
 app.use("/api", router);
--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -3,7 +3,7 @@ import { UserController } from './user.controller';
 
 const router = express.Router();
 router.post(
-    '/auth/signup',
+    '/signup',
     // auth(USER_ROLE.admin),
     // validateRequest(createStudentValidationSchema),
     UserController.createUser,
