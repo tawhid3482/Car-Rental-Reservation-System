3e9c2ddef7e2ec19c96295b1e2eff01f63e874e5
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-18 20:12:06 +0600

    booking model

--- src/app/modules/Booking/booking.model.ts
+++ src/app/modules/Booking/booking.model.ts
@@ -1,33 +1,31 @@
-import mongoose, { Schema } from "mongoose";
-import { TBooking } from "./booking.interface";
+import mongoose, { Schema, Document } from 'mongoose';
+import { TBooking } from './booking.interface';
 
-const BookingSchema: Schema = new Schema<TBooking>({
+export interface BookingDocument extends TBooking, Document {}
+
+const BookingSchema: Schema = new Schema({
   date: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  car: { type: Schema.Types.ObjectId, ref: "Car", required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  car: { type: Schema.Types.ObjectId, ref: 'Car', required: true },
   startTime: {
     type: String,
     required: true,
     validate: {
-      validator: function(v: string) {
-        return /^([01]\d|2[0-3]):([0-5]\d)$/.test(v);
-      },
-      message: props => `${props.value} is not a valid 24-hour time format (HH:mm)`
-    }
+      validator: (v: string) => /^([01]\d|2[0-3]):([0-5]\d)$/.test(v),
+      message: (props: { value: string }) => `${props.value} is not a valid 24-hour time format (HH:mm)`,
+    },
   },
   endTime: {
     type: String,
     required: true,
     validate: {
-      validator: function(v: string) {
-        return /^([01]\d|2[0-3]):([0-5]\d)$/.test(v);
-      },
-      message: props => `${props.value} is not a valid 24-hour time format (HH:mm)`
-    }
+      validator: (v: string) => /^([01]\d|2[0-3]):([0-5]\d)$/.test(v),
+      message: (props: { value: string }) => `${props.value} is not a valid 24-hour time format (HH:mm)`,
+    },
   },
   totalCost: { type: Number, default: 0 },
 });
 
-const BookingModel = mongoose.model<TBooking>("Booking", BookingSchema);
+const BookingModel = mongoose.model<BookingDocument>('Booking', BookingSchema);
 
 export default BookingModel;
